719bc9f633c8cb42d15b103f4db106cb32a896f6
HuelgaBar <huelgabar@gmail.com>
2025-02-01 18:09:36 -0300

    Add files via upload

--- docs/JS/menu.js
+++ docs/JS/menu.js
@@ -18,6 +18,9 @@ let $seccion = "",
   clase = "A",
   art,
   hh = "",
+  tit="",
+  sticky="",
+visible="",
   arthh = "";
 
 function llamarDb(url) {
@@ -30,15 +33,16 @@ function llamarDb(url) {
   )
     .then((resp) => resp.json())
     .then((dato) => {
-      console.log(dato)
+      // console.log(dato)
       // console.log(dato.record.menu);
       const data = dato; //orig = dato.record
       // console.log(data)
       // Empezando a armar la estructura de datos
       for (const key in data) {
+        // console.log(data[key])
         const seccionhh = document.createElement("section");
         seccionhh.classList.add("seccion1");
-        seccionhh.classList.add("visible");
+        seccionhh.classList.add("siempreActiva");
         let clase = key.replaceAll(" ", "_");
         seccionhh.classList.add(`${clase}`);
         const titulohh = document.createElement("h3");
@@ -47,9 +51,13 @@ function llamarDb(url) {
 
         const seccion = document.createElement("section");
         seccion.classList.add("seccion1");
+        const access=document.createElement("a")
+        access.href=`#${clase}`
         const titulo = document.createElement("h3");
         titulo.classList.add("seccion");
+        titulo.id= `${clase}`
         titulo.innerHTML = `${key}`;
+        access.appendChild(titulo)
         if (key != "id") {
           let clase2 = key.replaceAll(" ", "_");
           seccion.innerHTML = `<div class= 'pictures'><img src='./imagenes/${clase2}.jpg' alt='${key}'></div>`;
@@ -60,10 +68,9 @@ function llamarDb(url) {
             // console.log(datos);
 
             $precio = "";
-
             //Armando el arreglo
             datos[2].forEach((element, i) => {
-              
+            
                 $tipos += `<div>
               <span class="tipo">${element[0]}</span>
               <span class="precio white-space: pre;">${element[1]}</span>
@@ -80,9 +87,11 @@ function llamarDb(url) {
               </div>`;
               }
             });
-            art = `
-            <div class="articulo1">
-            <h4 class="articulo">${datos[0]}</h4>
+            tit = `${datos[0].replaceAll(" ", "_")}`;
+            if (data[key].length===1){
+              visible="class=' articulo1 siempreActiva' "} else{
+              visible ="class=articulo1"};
+            art = `<div><h4  class="articulo">${datos[0]}</h4></div><div ${visible}>
             <div class="descripcion">${datos[1]}</div>
             <div class="tipos">${$tipos}</div>
             </div>
@@ -106,7 +115,7 @@ function llamarDb(url) {
             // console.log(datos[2][0][1]);
             seccion.innerHTML += art;
 
-            document.querySelector(".menu").appendChild(titulo);
+            document.querySelector(".menu").appendChild(access);
             document.querySelector(".menu").appendChild(seccion);
           }
         }
